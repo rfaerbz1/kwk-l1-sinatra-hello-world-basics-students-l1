2c71877860bae39a62544501bb7ea5045158e7f8
rfaerbz1 <bfaerber@cox.net>
2018-06-13 04:00:45 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,5 +1,11 @@
 class App < Sinatra::Base
 
   # Define your GET '/' route below and respond with "Hello, World!"
-
+  get '/' do
+    "Hello, World!"
+  end 
+  get '/thing' do 
+    "ta ti ti ta"
+    erb :index
+  end 
 end
